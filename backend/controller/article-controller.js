3b04addfdda7e64d6404656bb83e4c6c3a1e4d21
Abdelfatah Ashour <abdelfatahashour4@gmail.com>
2022-03-02 20:45:38 +0200

    add new features in backend

--- backend/controller/article-controller.js
+++ backend/controller/article-controller.js
@@ -104,7 +104,7 @@ async function getOneArticleWithId(req, res) {
 
     return res.status(200).json({ message: result });
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(404).json({ message: "Article not found!" });
   }
 }
 
