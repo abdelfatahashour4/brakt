6f356c443d9dfd8e6e2eee26f398601817dda7ed
unknown <abdelfatahashour4@gmail.com>
2022-03-25 19:18:12 +0200

    ADD - api url in enviroment keys

--- client/src/utilities/axios.js
+++ client/src/utilities/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 const apiAxios = axios.create({
   baseURL:
-    process.env.NODE_ENV !== "production"
-      ? "/"
-      : process.env.REACT_APP_API_URL_PROD,
+    process.env.NODE_ENV !== "production" ? "/" : process.env.REACT_APP_API,
   withCredentials: true,
 });
 
